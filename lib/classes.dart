1397369338f5bd11b9fc2fd248631cab1b734e21
mouhamedmisou <mouhamedmisou@gmail.com>
2025-09-01 10:54:56 +0100

    starting work !

---
+++ lib/classes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static Color primary = Colors.blueGrey;
+  static Color secondary = Colors.orangeAccent;
+  static Color third = Colors.white;
+  static Color fourth = Colors.blue;
+  static Color fifth = Colors.deepPurple;
+}
